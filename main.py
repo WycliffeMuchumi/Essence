b9b8f22a1e21b11b963f2c2e150ee21096392ad9
WycliffeMuchumi <muchumimaina94@gmail.com>
2019-12-03 20:14:48 -0800

    regular updates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from configs.config import Development, Production
 import pygal
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 # configs
 
-app.config.from_object(Development)
+app.config.from_object(Production)
 
 # sqlAlchemy instance
 
@@ -33,9 +33,8 @@ def home():
     pie_chart.add('service', InventoryModel.getTypeCount("service"))
     graph_data = pie_chart.render_data_uri()
 
-
-    # conn = psycopg2.connect("dbname='d36lve8t356t1v' user='blufcyfuephvbf' host='ec2-176-34-184-174.eu-west-1.compute.amazonaws.com' password='f6bb9cce21036c899c21ea893eb19ef5568e2ef2c22316547c6ee5f3f149206a'")
-    conn = psycopg2.connect("dbname='Essence' user='postgres' host='localhost' password='12121994'")
+    conn = psycopg2.connect("dbname='d36lve8t356t1v' user='blufcyfuephvbf' host='ec2-176-34-184-174.eu-west-1.compute.amazonaws.com' password='f6bb9cce21036c899c21ea893eb19ef5568e2ef2c22316547c6ee5f3f149206a'")
+    # conn = psycopg2.connect("dbname='Essence' user='postgres' host='localhost' password='12121994'")
 
     cur = conn.cursor()
     cur.execute("""SELECT(sum(i.buyingprice * s.quantity)) as subtotal,(EXTRACT(MONTH FROM s.time_created)) as sales_month
@@ -63,9 +62,6 @@ ORDER BY sales_month""")
     linec.add('SubTotal', y)
     linec = linec.render_data_uri()
 
-
-
-
     return render_template('index.html', graph_data=graph_data, line_data=line_data, linec=linec)
 
 
@@ -83,21 +79,22 @@ def inventory():
 @app.route('/add', methods=['POST', 'GET'])
 def insert_inventory():
     if request.method == 'POST':
-        pn = request.form['product_name']
-        pt = request.form['product_type']
-        sn = request.form['serial_no']
-        bp = request.form['buying_price']
-        sp = request.form['selling_price']
-        s = request.form['stock']
-        rp = request.form['reorder_point']
-        print(pn)
-        print(pt)
-        print(sn)
-        print(sp)
-        print(s)
-        print(rp)
-        product_one = InventoryModel(productname=pn, producttype=pt, serialnumber=sn, buyingprice=bp, sellingprice=sp,
-                                     stock=s, reorderpoint=rp)
+        productname = request.form['product_name']
+        producttype = request.form['product_type']
+        serialnumber = request.form['serial_no']
+        buyingprice = request.form['buying_price']
+        sellingprice = request.form['selling_price']
+        stock = request.form['stock']
+        reorderpoint = request.form['reorder_point']
+        print(productname)
+        print(producttype)
+        print(serialnumber)
+        print(sellingprice)
+        print(stock)
+        print(reorderpoint)
+        product_one = InventoryModel(productname=productname, producttype=producttype, serialnumber=serialnumber,
+                                     buyingprice=buyingprice, sellingprice=sellingprice,
+                                     stock=stock, reorderpoint=reorderpoint)
         product_one.create_record()
 
         return redirect(url_for('inventory'))
@@ -105,25 +102,44 @@ def insert_inventory():
         return render_template('inventory.html')
 
 
-@app.route('/edit/<int:id>')
-def edit(id):
-    inventories = InventoryModel.get(inventory.id == id)
-    return render_template("inventory.html", inventories=inventories)
+# this route edits an inventory
 
-@app.route('/save/<int:id>', methods=['POST'])
-def save(id):
-    inventories = InventoryModel.get(inventory.id==id)
-    if request.method=="POST":
-        inventory.ProductName = request.form['product_name']
-        inventory.ProductType = request.form['product_type']
-        inventory.SerialNumber = request.form['serial_no']
-        inventory.BuyingPrice = request.form['buyingprice']
-        inventory.SellingPrice = request.form['sellingprice']
-        inventory.Stock = request.form['stock']
-        inventory.ReorderPoint = request.form['reorder_point']
-        inventory.save()
-        return redirect(url_for('inventory'))
 
+@app.route('/inventory/edit/<int:id>', methods=['POST'])
+def edit_inventory(id):
+    if request.method == 'POST':
+        productname = request.form['product_name']
+        producttype = request.form['product_type']
+        serialnumber = request.form['serial_no']
+        buyingprice = request.form['buying_price']
+        sellingprice = request.form['selling_price']
+        stock = request.form['stock']
+        reorderpoint = request.form['reorder_point']
+
+        update = InventoryModel.edit_inventory_by_id(id=id, productname=productname, producttype=producttype,
+                                                     serialnumber=serialnumber, buyingprice=buyingprice,
+                                                     sellingprice=sellingprice, stock=stock, reorderpoint=reorderpoint)
+
+        if update:
+            flash('successfully updated', 'success')
+            return redirect(url_for('inventory'))
+        else:
+            flash('update unsuccessful', 'danger')
+            return redirect(url_for('inventory'))
+
+
+# the  route below deletes an inventory
+
+
+@app.route('/inventory/delete/<int:id>', methods=['POST'])
+def delete_inventory(id):
+        delete = InventoryModel.delete_by_id(id)
+        if delete:
+            flash('inventory successfully delete', 'success')
+            return redirect(url_for('inventory'))
+        else:
+            flash('This inventory has already been reimbursed', 'danger')
+            return redirect(url_for('inventory'))
 
 
 @app.route('/sell', methods=['POST', 'GET'])
@@ -138,9 +154,6 @@ def sales():
         InventoryModel.update_stock(int(inventoryid), int(quantity))
 
     return redirect(url_for('inventory'))
-
-
-
 
 
 @app.route('/view_sales/<int:id>', methods=['GET'])
--- models.py
+++ models.py
@@ -22,9 +22,11 @@ class InventoryModel(db.Model):
         db.session.add(self)
         db.session.commit()
 
+
     @classmethod
     def get_inventory_by_id(cls, id):
         return InventoryModel.query.filter_by(id=id).first()
+
 
     # we first need to fetch the stock to be updated,update stock and pass the id and quantity
     @classmethod
@@ -35,10 +37,42 @@ class InventoryModel(db.Model):
             db.session.commit()
             return True
 
+
     @classmethod
     def getTypeCount(cls,name):
         record = cls.query.filter_by(producttype=name).count()
         return record
+
+    #edit
+    @classmethod
+    def edit_inventory_by_id(cls,id,productname=productname,producttype=producttype, serialnumber=serialnumber, buyingprice=buyingprice, sellingprice=sellingprice, stock=stock, reorderpoint=reorderpoint):
+        record = cls.query.filter_by(id=id).first()
+        if record:
+            record.productname = productname
+            record.producttype=producttype
+            record.serialnumber=serialnumber
+            record.buyingprice=buyingprice
+            record.sellingprice=sellingprice
+            record.stock=stock
+            record.reorderpoint=reorderpoint
+            db.session.commit()
+            return True
+        else:
+            return False
+
+    #delete
+    @classmethod
+    def delete_by_id(cls,id):
+        record=cls.query.filter_by(id=id)
+        if record.first():
+            record.delete()
+            db.session.commit()
+            return True
+        else:
+            return False
+
+
+
 
 from datetime import datetime
 class SalesModel(db.Model):
@@ -47,6 +81,7 @@ class SalesModel(db.Model):
     inventoryid = db.Column(db.Integer, db.ForeignKey('inventories.id'), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     time_created = db.Column(db.DateTime, default=datetime.utcnow)
+
 
     def create_record(self):
         db.session.add(self)
